30fa0163dc9aeb2a51ee8726b235e56a491c7e32
nikito7 <45373783+nikito7@users.noreply.github.com>
2020-09-28 14:19:53 +0100

    Update Tasmota-Removed.js

--- Tasmota-Removed.js
+++ Tasmota-Removed.js
@@ -1,5 +1,7 @@
 1,010410UUuuUUuuxx@i10:1,Potência Pico Média,W,Power_Max_P1,0
 
+1,010406uuxx@i9:1,Tarifa Activa,,Tariff_P1,16
+
 1,010416xxxxxxxxxxuuxxxxxx@i11:1,Clock H,h,Clock_H,16
 1,010416xxxxxxxxxxxxuuxxxx@i11:1,Clock M,m,Clock_M,16
 1,010416xxxxxxxxxxxxxxuuxx@i11:1,Clock S,s,Clock_S,16
