f26fe9233c21b4f91d50025774d534b809317f4e
nikito7 <45373783+nikito7@users.noreply.github.com>
2020-10-05 16:51:28 +0100

    Raw

--- Tasmota-Script.js
+++ Tasmota-Script.js
@@ -8,7 +8,7 @@ tper=60
 
 >M 1
 
-+1,14,m,1,9600,EB,5,15,01040001,r0104006C0006,,,01040079,01040073,01040075,01040077,r0104007B0005,,,r010400260003,0104002C,01040016,01040018,0104001A
++1,14,m,1,9600,EB,5,15,01040001,r0104006C0006,01040079,01040073,01040075,01040077,r0104007B0005,r010400260003,0104002C,01040016,r010400180004
 
 ; 01
 
@@ -31,43 +31,45 @@ tper=60
 
 ; 79 73 75 77
 
-1,010408UUuuUUuu@i4:1,Potência Activa ,W,Power,16
-1,010408UUuuUUuu@i5:1,Potência L1 ,W,Power_L1,16
-1,010408UUuuUUuu@i6:1,Potência L2 ,W,Power_L2,16
-1,010408UUuuUUuu@i7:1,Potência L3 ,W,Power_L3,16
+1,010408UUuuUUuu@i2:1,Potência Activa ,W,Power,16
+1,010408UUuuUUuu@i3:1,Potência L1 ,W,Power_L1,16
+1,010408UUuuUUuu@i3:1,Potência L2 ,W,Power_L2,16
+1,010408UUuuUUuu@i5:1,Potência L3 ,W,Power_L3,16
 
 1,=h<hr>
 
 ; 0104-007B-0005
 
-1,01040aUUuu@i8:1000,Factor de Potência ,pu,PFactor,18
-1,01040axxxxUUuu@i8:1000,PFactor L1 ,pu,PFactor_L1,18
-1,01040axxxxxxxxUUuu@i8:1000,PFactor L2 ,pu,PFactor_L2,18
-1,01040axxxxxxxxxxxxUUuu@i8:1000,PFactor L3 ,pu,PFactor_L3,18
-1,01040axxxxxxxxxxxxxxxxUUuu@i8:10,Frequência ,Hz,Frequency,17
+1,01040aUUuu@i6:1000,Factor de Potência ,pu,PFactor,18
+1,01040axxxxUUuu@i6:1000,PFactor L1 ,pu,PFactor_L1,18
+1,01040axxxxxxxxUUuu@i6:1000,PFactor L2 ,pu,PFactor_L2,18
+1,01040axxxxxxxxxxxxUUuu@i6:1000,PFactor L3 ,pu,PFactor_L3,18
+1,01040axxxxxxxxxxxxxxxxUUuu@i6:10,Frequência ,Hz,Frequency,17
 
 1,=h<hr>
 
 ; 0104-0026-0003
 
-1,01040cUUuuUUuu@i11:1000,Energia T1 Vazio ,kWh,Energy_R1,16
-1,01040cxxxxxxxxUUuuUUuu@i11:1000,Energia T2 Ponta ,kWh,Energy_R2,16
-1,01040cxxxxxxxxxxxxxxxxUUuuUUuu@i11:1000,Energia T3 Cheia ,kWh,Energy_R3,16
+1,01040cUUuuUUuu@i7:1000,Energia T1 Vazio ,kWh,Energy_R1,16
+1,01040cxxxxxxxxUUuuUUuu@i7:1000,Energia T2 Ponta ,kWh,Energy_R2,16
+1,01040cxxxxxxxxxxxxxxxxUUuuUUuu@i7:1000,Energia T3 Cheia ,kWh,Energy_R3,16
 
 ; 2C
 
-1,010408UUuuUUuu@i12:1000,Energia Total ,kWh,Energy_Sum,16
+1,010408UUuuUUuu@i8:1000,Energia Total ,kWh,Energy_Sum,16
 
 1,=h<hr>
 
 ; 16
 
-1,010408xxxxxxxxUUuuUUuu@i13:1000,Energia Exportada ,kWh,Energy_Export,19
+1,010408xxxxxxxxUUuuUUuu@i9:1000,Energia Exportada ,kWh,Energy_Export,19
 
-; 18 1A
+; 0104-0018-0004
 
-1,010408UUuuUUuuxxxxxxxx@i14:1000,Energia Q1 +Ri ,kVAr,Energy_Q1,19
-1,010408xxxxxxxxUUuuUUuu@i15:1000,Energia Q4 -Rc ,kVAr,Energy_Q4,19
+1,010410UUuuUUuu@i10:1000,Energia Q1 +Ri ,kVAr,Energy_Q1,19
+1,010410xxxxxxxxUUuuUUuu@i10:1000,Energia Q2 +Rc ,kVAr,Energy_Q2,19
+1,010410xxxxxxxxxxxxxxxxUUuuUUuu@i10:1000,Energia Q3 -Ri ,kVAr,Energy_Q3,19
+1,010410xxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i10:1000,Energia Q4 -Rc ,kVAr,Energy_Q4,19
 
 ; dev
 
