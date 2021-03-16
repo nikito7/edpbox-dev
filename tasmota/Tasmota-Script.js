74265a3f7158b85d1be01e290c893c765c661183
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-03-16 03:15:40 +0000

    Update Tasmota-Script.js

--- tasmota/Tasmota-Script.js
+++ tasmota/Tasmota-Script.js
@@ -1,11 +1,9 @@
 >D
 
-x16=xxxxxxxxxxxxxxxx
-
 >BS
 
 smlj=0
-tper=60
+tper=30
 
 ->sensor53 r
 
@@ -15,16 +13,16 @@ smlj=0
 
 >S
 
-if upsecs>30
+if upsecs>25
 then
 smlj=1
 endif
 
 >M 1
 
-+1,14,m,1,9600,EB1,5,15,r010400010001,r0104006C0007,r010400730007,r0104007B0005,r010400260003,r010400160002,r010400180004
++1,14,m,1,9600,EB1,5,9,r010400010001,r0104006C0007,r010400730007,r0104007B0005,r010400260003,r010400160006
 
-; 01 count 1
+; 01
 
 1,01040Cxxxxxxxxxxuu@i0:1,Clock ,h,Clock_H,16
 1,01040Cxxxxxxxxxxxxuu@i0:1,Clock ,m,Clock_M,16
@@ -32,7 +30,7 @@ endif
 
 1,=h<hr>
 
-; 6C count 7
+; 6C
 
 1,01040EUUuu@i1:10,Tensão L1 ,V,Voltage_L1,1
 1,01040ExxxxxxxxUUuu@i1:10,Tensão L2 ,V,Voltage_L2,1
@@ -47,7 +45,7 @@ endif
 
 1,=h<hr>
 
-; 73 count 7
+; 73
 
 1,01041cUUuuUUuu@i2:1,Potência L1 ,W,Power_L1,16
 1,01041cxxxxxxxxxxxxxxxxUUuuUUuu@i2:1,Potência L2 ,W,Power_L2,16
@@ -56,7 +54,7 @@ endif
 
 1,=h<hr>
 
-; 7B count 5
+; 7B
 
 1,01040AUUuu@i3:1000,Factor de Potência ,pu,PFactor,19
 1,01040AxxxxUUuu@i3:1000,PFactor L1 ,pu,PFactor_L1,19
@@ -66,7 +64,7 @@ endif
 
 1,=h<hr>
 
-; 26 count 3
+; 26
 
 1,01040CUUuuUUuu@i4:1000,Energia T1 Vazio ,kWh,Energy_R1,1
 1,01040CxxxxxxxxUUuuUUuu@i4:1000,Energia T2 Ponta ,kWh,Energy_R2,1
@@ -74,17 +72,14 @@ endif
 
 1,=h<hr>
 
-; 16 count 2
+; 16
 
-1,010408UUuuUUuu@i5:1000,Energia Importada ,kWh,Energy_Sum,1
-1,010408xxxxxxxxUUuuUUuu@i5:1000,Energia Exportada ,kWh,Energy_Export,3
-
-; 18 count 4
-
-1,010410UUuuUUuu@i6:1000,Energia Q1 +Ri ,kVAr,Energy_Q1,1
-1,010410xxxxxxxxUUuuUUuu@i6:1000,Energia Q2 ,kVAr,*,4
-1,010410xxxxxxxxxxxxxxxxUUuuUUuu@i6:1000,Energia Q3 ,kVAr,*,4
-1,010410xxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1000,Energia Q4 +Rc ,kVAr,Energy_Q4,2
+1,010418UUuuUUuu@i5:1000,Energia Importada ,kWh,Energy_Sum,1
+1,010418xxxxxxxxUUuuUUuu@i5:1000,Energia Exportada ,kWh,Energy_Export,3
+1,010418xxxxxxxxxxxxxxxxUUuuUUuu@i6:1000,Energia Q1 +Ri ,kVAr,Energy_Q1,1
+1,010418xxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1000,Energia Q2 ,kVAr,*,4
+1,010418xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1000,Energia Q3 ,kVAr,*,4
+1,010418xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1000,Energia Q4 +Rc ,kVAr,Energy_Q4,2
 
 ; dev
 
