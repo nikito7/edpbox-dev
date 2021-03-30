073c85e9bc762527331a97b7ebeae6ef27eec935
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-03-30 20:46:21 +0100

    Create test2.js

---
+++ tasmota/notes/test2.js
@@ -0,0 +1,22 @@
+>D
+>B
+=>sensor53 r
+
+>M 1
+
++1,3,mN1,1,9600,test2,1,15,r010400010001,r0104006C0001,r010400730001,r0104007B0001,r010400260001,r010400160001
+
+1,01040Cxxxxxxxxxxuu@i0:1,Clock ,h,Clock_H,16
+
+1,010402UUuu@i1:10,Voltage L1 ,V,VolL1,17
+
+1,010404UUuuUUuu@i2:1,Active Power L1 ,W,ActPowL1,16
+@i2:1,Active Power L3 ,W,ActPowL3,16
+
+1,010402UUuu@i3:1000,Power Factor ,pu,PowFac,19
+
+1,010404UUuuUUuu@i4:1000,Total Energy T1 Vazio ,kWh,TotEneT1,17
+
+1,010404UUuuUUuu@i5:1000,Total Energy Import ,kWh,TotEneImp,1
+
+#
