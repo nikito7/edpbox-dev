6a6ad1e00c1f7338784ce6151ec8064473952d01
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-05-25 13:31:28 +0100

    Update script-eb2mono.js

--- tasmota/script-eb2mono.js
+++ tasmota/script-eb2mono.js
@@ -41,33 +41,28 @@ print script: up %upsecs%
 
 ; 6C
 
-1,01040EUUuu@i1:10,Voltage L1 ,V,VolL1,17
-
-1,01040ExxxxxxxxxxxxxxxxxxxxxxxxUUuu@i1:10,Current ,A,Current,17
-
-1,01040ExxxxUUuu@i1:10,Current L1 ,A,CurL1,17
+1,01040EUUuu@i1:10,Voltage L1 ,V,VolL1,18
+1,01040ExxxxUUuu@i1:10,Current L1 ,A,CurL1,18
 
 ; 73
 
-1,01041cxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i2:1,Active Power ,W,ActPow,16
-
-1,01041cUUuuUUuu@i2:1,Active Power L1 ,W,ActPowL1,19
+1,01041cUUuuUUuu@i2:1,Active Power L1 ,W,ActPowL1,20
 
 1,=h<hr>
 
 ; 7B
 
-1,01040AUUuu@i3:1000,Power Factor ,pu,PowFac,19
-1,01040AxxxxUUuu@i3:1000,Power Factor L1 ,pu,PowFacL1,19
-1,01040AxxxxxxxxxxxxxxxxUUuu@i3:10,Frequency ,Hz,Frequency,1
+1,01040AUUuu@i3:1000,Power Factor ,pu,PowFac,20
+1,01040AxxxxUUuu@i3:1000,Power Factor L1 ,pu,PowFacL1,20
+1,01040AxxxxxxxxxxxxxxxxUUuu@i3:10,Frequency ,Hz,Frequency,2
 
 1,=h<hr>
 
 ; 26
 
-1,01040CUUuuUUuu@i4:1000,Total Energy T1 Vazio ,kWh,TotEneT1,1
-1,01040CxxxxxxxxUUuuUUuu@i4:1000,Total Energy T2 Ponta ,kWh,TotEneT2,1
-1,01040CxxxxxxxxxxxxxxxxUUuuUUuu@i4:1000,Total Energy T3 Cheias ,kWh,TotEneT3,1
+1,01040CUUuuUUuu@i4:1000,Total Energy T1 Vazio ,kWh,TotEneT1,2
+1,01040CxxxxxxxxUUuuUUuu@i4:1000,Total Energy T2 Ponta ,kWh,TotEneT2,2
+1,01040CxxxxxxxxxxxxxxxxUUuuUUuu@i4:1000,Total Energy T3 Cheias ,kWh,TotEneT3,2
 
 1,=h<hr>
 
