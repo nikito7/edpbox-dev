39382020f7dcb3f1f4c2a2a1a88101dac6329a0f
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-05-25 14:24:00 +0100

    Update script-eb2mono.js

--- tasmota/script-eb2mono.js
+++ tasmota/script-eb2mono.js
@@ -15,12 +15,10 @@ smlj=0
 
 >S
 
-if upsecs==55
+if upsecs==50
 then
 smlj=1
 endif
-
-->LedPower 2
 
 print script: up %upsecs%
 
@@ -29,7 +27,7 @@ print script: up %upsecs%
 ; change to your gpios and mode
 ; power off is required 
 ;  v   v             v
-+1,18,mN1,1,9600,EB2,19,15,r010400010001,r0104006C0007,r010400730007,r0104007B0005,r010400260003,r010400160006
++1,18,mN1,1,9600,EB2,19,9,r010400010001,r0104006C0002,r010400160002,r010400260003,r010400790003,r0104007F0002
 
 ; 01
 
@@ -41,35 +39,35 @@ print script: up %upsecs%
 
 ; 6C
 
-1,01040EUUuu@i1:10,Voltage L1 ,V,VolL1,18
-1,01040ExxxxUUuu@i1:10,Current L1 ,A,CurL1,18
-
-; 73
-
-1,01041cUUuuUUuu@i2:1,Active Power L1 ,W,ActPowL1,20
-
-1,=h<hr>
-
-; 7B
-
-1,01040AUUuu@i3:1000,Power Factor ,pu,PowFac,20
-1,01040AxxxxUUuu@i3:1000,Power Factor L1 ,pu,PowFacL1,20
-1,01040AxxxxxxxxxxxxxxxxUUuu@i3:10,Frequency ,Hz,Frequency,2
-
-1,=h<hr>
-
-; 26
-
-1,01040CUUuuUUuu@i4:1000,Total Energy T1 Vazio ,kWh,TotEneT1,2
-1,01040CxxxxxxxxUUuuUUuu@i4:1000,Total Energy T2 Ponta ,kWh,TotEneT2,2
-1,01040CxxxxxxxxxxxxxxxxUUuuUUuu@i4:1000,Total Energy T3 Cheias ,kWh,TotEneT3,2
+1,010404UUuu@i1:10,Voltage L1 ,V,VolL1,17
+1,010404xxxxUUuu@i1:10,Current L1 ,A,CurL1,18
 
 1,=h<hr>
 
 ; 16
 
-1,010418UUuuUUuu@i5:1000,Total Energy Import ,kWh,TotEneImp,2
-1,010418xxxxxxxxUUuuUUuu@i5:1000,Total Energy Export ,kWh,TotEneExp,3
+1,010408UUuuUUuu@i2:1000,Total Energy Import ,kWh,TotEneImp,3
+1,010408xxxxxxxxUUuuUUuu@i2:1000,Total Energy Export ,kWh,TotEneExp,3
+
+1,=h<hr>
+
+; 26
+
+1,01040CUUuuUUuu@i3:1000,Total Energy T1 Vazio ,kWh,TotEneT1,2
+1,01040CxxxxxxxxUUuuUUuu@i3:1000,Total Energy T2 Ponta ,kWh,TotEneT2,2
+1,01040CxxxxxxxxxxxxxxxxUUuuUUuu@i3:1000,Total Energy T3 Cheias ,kWh,TotEneT3,2
+
+1,=h<hr>
+
+; 79
+
+1,01040aUUuuUUuu@i4:1,Active Power ,W,ActPow,16
+1,01040axxxxxxxxUUuuUUuu@i4:1,Active Power Export ,W,ActPowExp,16
+1,01040axxxxxxxxxxxxxxxxUUuu@i4:1000,Power Factor ,pu,PowFac,19
+
+; 7F
+
+1,01040aUUuu@i5:10,Frequency ,Hz,Frequency,2
 
 #
 
