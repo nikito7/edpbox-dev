5a4a067f57c538d7386cb215f5d7aa45c58f7c32
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-09-10 22:12:14 +0100

    Update Script-EB3.js

--- tasmota/Script-EB3.js
+++ tasmota/Script-EB3.js
@@ -83,10 +83,10 @@ endif
 1,010418UUuuUUuu@i5:1000,Total Energy Import ,kWh,TotEneImp,1
 1,010418xxxxxxxxUUuuUUuu@i5:1000,Total Energy Export ,kWh,TotEneExp,3
 
-1,010418xxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q1 ,kVAr,TotEneQ1,3
+1,010418xxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q1 ,kVAr,TotEneQ1,2
 1,010418xxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q2 ,kVAr,TotEneQ2,3
 1,010418xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q3 ,kVAr,TotEneQ3,3
-1,010418xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q4 ,kVAr,TotEneQ4,3
+1,010418xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q4 ,kVAr,TotEneQ4,2
 
 1,=h<hr>
 
