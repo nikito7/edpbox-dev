06add3ae016aee476889ebecb6298037e3fde34c
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-09-19 15:09:53 +0100

    Update Script-EB3-trash.js

--- tasmota/Script-EB3-trash.js
+++ tasmota/Script-EB3-trash.js
@@ -1,5 +1,18 @@
 ###
 
+; 16
+
+1,010418UUuuUUuu@i5:1000,Total Energy Import ,kWh,TotEneImp,1
+1,010418xxxxxxxxUUuuUUuu@i5:1000,Total Energy Export ,kWh,TotEneExp,3
+
+1,010418xxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q1 ,kVAr,TotEneQ1,2
+1,010418xxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q2 ,kVAr,TotEneQ2,3
+1,010418xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q3 ,kVAr,TotEneQ3,3
+1,010418xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q4 ,kVAr,TotEneQ4,2
+
+
+###
+
 ; 0B
 
 1,01040euu@i6:1,Tariff ,,Tariff,16
