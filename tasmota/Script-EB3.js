4d6f9e79a579866ac53ef3a02a31a9a3e7cef5c1
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-09-19 15:00:46 +0100

    Update Script-EB3.js

--- tasmota/Script-EB3.js
+++ tasmota/Script-EB3.js
@@ -113,20 +113,6 @@ ss=sml[3]
 1,01040exxxxxxxxxxUUuuUUuu@i6:1000,T2 Max Ponta ,kVA,ConT2,2
 1,01040exxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1000,T3 Max Cheias ,kVA,ConT3,2
 
-1,=h<hr>
-
-; 84
-
-1,01040auu@i7:1,ICP Status ,,ICP_Status,0
-1,01040axxUUuuUUuu@i7:1,ICP Q ,,ICP_Q,0
-1,01040axxxxxxxxxxUUuuUUuu@i7:1,ICP K ,%%,ICP_K,0
-
-1,=h<hr>
-
-; 12
-
-1,=h<small>Potencia Contratada</small>
-1,010404UUuuUUuu@i8:1000,Max Power ,kVA,Max_Power,2
 
 1,=h<hr>
 
