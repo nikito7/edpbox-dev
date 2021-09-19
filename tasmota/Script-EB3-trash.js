b049c19280c6a3a1d87cc40d0223e5fb8763a017
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-09-19 15:02:11 +0100

    Create Script-EB3-trash.js

---
+++ tasmota/Script-EB3-trash.js
@@ -0,0 +1,18 @@
+1,=h<hr>
+
+; 84
+
+1,01040auu@i7:1,ICP Status ,,ICP_Status,0
+1,01040axxUUuuUUuu@i7:1,ICP Q ,,ICP_Q,0
+1,01040axxxxxxxxxxUUuuUUuu@i7:1,ICP K ,%%,ICP_K,0
+
+1,=h<hr>
+
+; 12
+
+1,=h<small>Potencia Contratada</small>
+1,010404UUuuUUuu@i8:1000,Max Power ,kVA,Max_Power,2
+
+###
+##
+#
