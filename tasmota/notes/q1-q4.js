968a78231dab32cc2775206d0926d4b587d118d5
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-09-19 21:26:19 +0100

    Delete q1-q4.js

--- tasmota/notes/q1-q4.js
+++
@@ -1,6 +0,0 @@
-1,010418xxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q1 ,kVAr,TotEneQ1,2
-1,010418xxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q2 ,kVAr,TotEneQ2,3
-1,010418xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q3 ,kVAr,TotEneQ3,3
-1,010418xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i5:1000,Total Energy Q4 ,kVAr,TotEneQ4,2
-
-;eof
