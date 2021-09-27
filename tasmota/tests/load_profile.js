9116af9e13719f982eacc226cadd1953306336a9
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-09-27 19:32:42 +0100

    Update load_profile.js

--- tasmota/tests/load_profile.js
+++ tasmota/tests/load_profile.js
@@ -9,9 +9,15 @@ smlj=0
 
 >M 1
 
-+1,19,mN1,0,9600,EB99,18,15,r010400820001
++1,19,mN1,0,9600,EB99,18,15,r010400820001,r010400830001
 
-1,010404UUuuUUuu@i0:1,Entries ,,*,0
+; 82
+
+1,010404UUuuUUuu@i0:1,Entries in Use ,,*,0
+
+; 83
+
+1,010404UUuuUUuu@i1:1,Profile Entries ,,*,0
 
 1,=h<hr>
 
