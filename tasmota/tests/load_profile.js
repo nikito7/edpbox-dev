51bc4b923f1ed468be375c44e7dd2a16fd4ed313
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-09-27 21:07:56 +0100

    Update load_profile.js

--- tasmota/tests/load_profile.js
+++ tasmota/tests/load_profile.js
@@ -3,12 +3,19 @@
 =>sensor53 r
 >M 1
 
-+1,19,mN1,0,9600,EB99,18,15,r010400820002,014503000011bc01
++1,19,mN1,0,9600,EB99,18,15,r01040080002,r010400820002,014503000011bc01
+
+; 80/81
+
+1,010412uu@i0:1,Config ID 1 ,,*,0
+1,010412xxuu@i0:1,Config ID  2 ,,*,0
+1,010412xxxxuu@i0:1,Config ID 3 ,,*,0
+1,010412xxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i0:1,Capture Period ,s,*,0
 
 ; 82/83
 
-1,010408UUuuUUuu@i0:1,Entries in Use ,,*,0
-1,010408xxxxxxxxUUuuUUuu@i0:1,Profile Entries ,,*,0
+1,010408UUuuUUuu@i1:1,Entries in Use ,,*,0
+1,010408xxxxxxxxUUuuUUuu@i1:1,Profile Entries ,,*,0
 
 1,=h<hr>
 1,=hExample:
