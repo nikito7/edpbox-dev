8f2aa9a656dc7020732801f3f1df436b092cb080
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-09-27 21:17:14 +0100

    Update load_profile.js

--- tasmota/tests/load_profile.js
+++ tasmota/tests/load_profile.js
@@ -3,28 +3,28 @@
 =>sensor53 r
 >M 1
 
-+1,19,mN1,0,9600,EB99,18,15,r010400800002,r010400820002,014503000011bc01
++1,19,mN1,0,9600,EB99,18,15,r010400800001,r010400810003,014503000011bc01
 
-; 80/81
+; 80
 
-1,010412uu@i0:1,Config ID 1 ,,*,0
-1,010412xxuu@i0:1,Config ID  2 ,,*,0
-1,010412xxxxuu@i0:1,Config ID 3 ,,*,0
-1,010412xxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i0:1,Capture Period ,s,*,0
+1,010408uu@i0:1,Config ID 1 ,,*,0
+1,010408xxuu@i0:1,Config ID  2 ,,*,0
+1,010408xxxxuu@i0:1,Config ID 3 ,,*,0
 
-; 82/83
+; 81/82/83
 
-1,010408UUuuUUuu@i1:1,Entries in Use ,,*,0
-1,010408xxxxxxxxUUuuUUuu@i1:1,Profile Entries ,,*,0
+1,01040cUUuuUUuu@i1:1,Capture Period ,s,*,0
+1,01040cxxxxxxxxUUuuUUuu@i1:1,Entries in Use ,,*,0
+1,01040cxxxxxxxxxxxxxxxxUUuuUUuu@i1:1,Profile Entries ,,*,0
 
 1,=h<hr>
 1,=hExample:
 
 1,014511UUuu@i2:1,Year ,,*,0
 
-1,014511xxxxxxxxxxuu@i2:1,Time Stamp ,h,*,0
-1,014511xxxxxxxxxxxxuu@i2:1,Time Stamp ,m,*,0
-1,014511xxxxxxxxxxxxxxuu@i2:1,Time Stamp ,s,*,0
+1,014511xxxxxxxxxxuu@i2:1,Timestamp ,h,*,0
+1,014511xxxxxxxxxxxxuu@i2:1,Timestamp ,m,*,0
+1,014511xxxxxxxxxxxxxxuu@i2:1,Timestamp ,s,*,0
 
 
 1,=h<hr>
