f726d761ba03cf3f4e7ba1fde1bf9eb23b2db9b5
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-09-30 13:15:52 +0100

    Update profile-3.js

--- tasmota/notes/profile-3.js
+++ tasmota/notes/profile-3.js
@@ -49,7 +49,7 @@ endif
 @ <b>Local Time: </b> %tstamp%
 @ <b>Name: </b> %name%
 @ <b>Clock: </b> %0hh%:%0mm%:%0ss%
-@ <b>UPAC: </b> "/%0upacyr%/%0upacmt%/%0upacdy%/%0upachh%/%0upacmm%/%0upacss%/%3upacim%/%3upacex%/"
+@ <b>UPAC: </b> %0upacyr%%0upacmt%%0upacdy%%0upachh%%0upacmm%%0upacss%IMP%3upacim%EXP%3upacex%ZZ
 @ <hr>
 
 >M 1
