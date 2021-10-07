76b1b6fbe0cd139ffe014153a0438e9b9e46c9ae
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-10-07 18:14:43 +0100

    Update build.h

--- tasmota/esp8266/build.h
+++ tasmota/esp8266/build.h
@@ -126,15 +126,25 @@
 #undef DEBUG_THEO                                // Disable debug code
 #undef USE_DEBUG_DRIVER                          // Disable debug code
 
+// user_config_override.h
 // sml configs
 
 #undef USE_RULES
 #define USE_SCRIPT
 #define USE_SML_M
-#define SML_MAX_VARS 50
+#define USE_SML_SCRIPT_CMD
+#define SML_MAX_VARS 75
+#define SML_REPLACE_VARS
 
 // tcp
 
 #define USE_TCP_BRIDGE
+
+// misc
+
+#define USE_SCRIPT_WEB_DISPLAY
+#define USE_SCRIPT_JSON_EXPORT
+#define MAX_METERS 1
+#define MAXSVARS 10
 
 // eof
