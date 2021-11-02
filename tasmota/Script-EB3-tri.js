a45ff6b3ea96976bcfb10af0a0d4c03708b01b03
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-11-02 17:28:32 +0000

    Update Script-EB3-tri.js

--- tasmota/Script-EB3-tri.js
+++ tasmota/Script-EB3-tri.js
@@ -117,13 +117,13 @@ endif
 1,=h<hr>
 1,=hLoad Profile: Lastest
 
-1,01441dUUuu@i7:1,Y ,,LP_Y,0
-1,01441dxxxxuu@i7:1,M ,,LP_M,0
-1,01441dxxxxxxuu@i7:1,D ,,LP_D,0
-1,01441dxxxxxxxxxxuu@i7:1,HH ,h,LP_HH,0
-1,01441dxxxxxxxxxxxxuu@i7:1,MM ,m,LP_MM,0
-1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i7:1000,Import Inc ,kWh,LP_IMP,3
-1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i7:1000,Export Inc ,kWh,LP_EXP,3
+1,01441dUUuu@i7:1,Y ,,LP_Y,16
+1,01441dxxxxuu@i7:1,M ,,LP_M,16
+1,01441dxxxxxxuu@i7:1,D ,,LP_D,16
+1,01441dxxxxxxxxxxuu@i7:1,HH ,h,LP_HH,16
+1,01441dxxxxxxxxxxxxuu@i7:1,MM ,m,LP_MM,16
+1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i7:1000,Import Inc ,kWh,LP_IMP,19
+1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i7:1000,Export Inc ,kWh,LP_EXP,19
 
 1,=h<hr>
 
