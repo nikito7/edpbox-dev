0f71915a412311f446908631c228ca87cb87b0ea
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-12-09 19:14:15 +0000

    Update build.h

--- tasmota/esp8266/4mb/build.h
+++ tasmota/esp8266/4mb/build.h
@@ -1,7 +1,6 @@
 // .ini
 // board = esp8266_4M2M
 
-
 // user_config_override.h
 
 // ufs
@@ -9,25 +8,7 @@
 #define USE_UFILESYS
 #define UFSYS_SIZE 8192
 
-// sml configs
-
-#undef USE_RULES
-#define USE_SCRIPT
-#define USE_SML_M
-#define USE_SML_SCRIPT_CMD
-#define SML_MAX_VARS 75
-#define SML_REPLACE_VARS
-
-// tcp
-
-#define USE_TCP_BRIDGE
-
-// misc
-
-#define USE_SCRIPT_WEB_DISPLAY
-#define USE_SCRIPT_JSON_EXPORT
-#define MAX_METERS 1
-#define MAXSVARS 10
-
+// (...)
+// same as 1MB
 // eof
 
