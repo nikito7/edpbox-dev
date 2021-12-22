cc24f61792f38a750f965fbe5e45edd03d6d5ffb
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-12-22 20:38:50 +0000

    Update Script-EB1-mono.js

--- tasmota/Script-EB1-mono.js
+++ tasmota/Script-EB1-mono.js
@@ -1,28 +1,85 @@
 >D
 
-; EB1 kaifa example
+; EB1
+
+bug="fix"
+time=""
+date=""
+cnt=0
+clk=0
+wtd=0
+old=0
 
 >B
 
 tper=60
 smlj=0
 
-=>sensor53 r
+=>Sensor53 r
 
 >S
 
-if upsecs==55
+if cnt==30
 then
-tper=21
+tper=10
 smlj=1
 endif
+
+if cnt==50
+then
+tper=60
+endif
+
+time=st(tstamp T 2)
+date=st(tstamp T 1)
+
+if cnt<99
+then
+cnt+=1
+endif
+
+; modbus watchdog
+
+clk=sml[3]
+
+if cnt==99
+then
+wtd+=1
+endif
+
+if wtd==1
+then
+old=sml[3]
+endif
+
+if wtd==30
+then
+wtd=0
+if old==clk
+then
+print modbus error
+; 
+=>Restart -3
+; 
+endif
+endif
+
+>W
+
+@<b>NTP: </b> %date% %time%
+@<b>Vars: </b> cnt=%0cnt% tper=%0tper% smlj=%0smlj%
+@<b>Vars: </b> wtd=%0wtd% clk=%0clk% old=%0old%
+@<hr>
 
 >M 1
 
 ; change to your gpios and mode
-; power off is required 
-;  v   v             v
-+1,19,mN1,1,9600,EB1,18,50,r010400010001,r0104006C0002,r010400160002,r010400260003,r010400790003,r0104007F0002,r0104000B0002,r0104002D0003
+; power off is required
+;  v  v             v
++1,3,mN1,1,9600,EB1,1,15,r010400010001,r0104006C0002,r010400160002,r010400260003,r010400790003,r0104007F0002,r0104000B0002,r0104002D0003,r01440601
+
+; import: r01440301
+; import+export: r01440601
 
 ; 01
 
@@ -75,6 +132,21 @@ endif
 1,01040CUUuuUUuu@i7:1000,Total Energy T1 Export ,kWh,TotEneT1Exp,3
 1,01040CxxxxxxxxUUuuUUuu@i7:1000,Total Energy T2 Export ,kWh,TotEneT2Exp,3
 1,01040CxxxxxxxxxxxxxxxxUUuuUUuu@i7:1000,Total Energy T3 Export ,kWh,TotEneT3Exp,3
+
+1,=h<hr>
+1,=hLoad Profile: Lastest
+
+; import: 11
+; import+export: 1d
+
+1,01441dUUuu@i8:1,Y ,,LP_Y,0
+1,01441dxxxxuu@i8:1,M ,,LP_M,0
+1,01441dxxxxxxuu@i8:1,D ,,LP_D,0
+1,01441dxxxxxxxxxxuu@i8:1,HH ,h,LP_HH,0
+1,01441dxxxxxxxxxxxxuu@i8:1,MM ,m,LP_MM,0
+1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i8:1000,Import Inc ,kWh,LP_IMP,3
+
+1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i8:1000,Export Inc ,kWh,LP_EXP,3
 
 #
 
