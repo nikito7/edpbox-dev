feb36ff6da18fa95a71c4e243c1cea40f829ceec
nikito7 <45373783+nikito7@users.noreply.github.com>
2021-12-30 14:06:29 +0000

    Update Script-EB3-tri.js

--- tasmota/Script-EB3-tri.js
+++ tasmota/Script-EB3-tri.js
@@ -143,18 +143,6 @@ endif
 
 1,01040euu@i6:1,Tariff ,,Tariff,16
 
-1,=h<hr>
-
-1,01441dUUuu@i7:1,Y ,,LP_Y,0
-1,01441dxxxxuu@i7:1,M ,,LP_M,0
-1,01441dxxxxxxuu@i7:1,D ,,LP_D,0
-1,01441dxxxxxxxxxxuu@i7:1,HH ,h,LP_HH,0
-1,01441dxxxxxxxxxxxxuu@i7:1,MM ,m,LP_MM,0
-1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i7:1,Import Inc ,Wh,LP_IMP,0
-1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i7:1,Export Inc ,Wh,LP_EXP,0
-
-1,=h<hr>
-
 #
 
 ; eof
