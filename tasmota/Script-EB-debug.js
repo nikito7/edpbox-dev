b97a97e0c6f79446021ae4defe97878ddb4d32d2
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-01-14 22:42:02 +0000

    Update Script-EB-debug.js

--- tasmota/Script-EB-debug.js
+++ tasmota/Script-EB-debug.js
@@ -9,10 +9,14 @@
 ; +1,14,mN1,1,9300,Test,5,30,r010400010001
 +1,3,mN1,1,9300,Test,1,30,r010400010001
 
+; console debug
+;
 ; sensor53 d1
 ; sensor53 d0
 
-1,01040cUUuu@i0:1,Test,yr,Test,0
+1,01040Cxxxxxxxxxxuu@i0:1,Clock ,h,*,0
+1,01040Cxxxxxxxxxxxxuu@i0:1,Clock ,m,*,0
+1,01040Cxxxxxxxxxxxxxxuu@i0:1,Clock ,s,*,0
 
 #
 
