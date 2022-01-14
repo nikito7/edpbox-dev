cb5b1f073f6f9e5216e5ac500a58fa551b9d5a4f
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-01-14 13:08:25 +0000

    Update build.h

--- tasmota/esp8266/4mb/build.h
+++ tasmota/esp8266/4mb/build.h
@@ -8,7 +8,25 @@
 #define USE_UFILESYS
 #define UFSYS_SIZE 8192
 
-// (...)
-// same as 1MB
-// eof
+// user_config_override.h
+// sml configs
 
+#undef USE_RULES
+#define USE_SCRIPT
+#define USE_SML_M
+#define USE_SML_SCRIPT_CMD
+#define SML_MAX_VARS 50
+#define SML_REPLACE_VARS
+
+// tcp
+
+//#define USE_TCP_BRIDGE
+
+// misc
+
+#define USE_SCRIPT_WEB_DISPLAY
+#define USE_SCRIPT_JSON_EXPORT
+#define MAX_METERS 2
+#define MAXSVARS 10
+
+// eof
