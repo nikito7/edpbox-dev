fb884518afce741a696abe1befd21643c504ed4a
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-01-15 18:09:51 +0000

    Update build.h

--- tasmota/esp32/build.h
+++ tasmota/esp32/build.h
@@ -5,18 +5,18 @@
 #define USE_SCRIPT
 #define USE_SML_M
 #define USE_SML_SCRIPT_CMD
-#define SML_MAX_VARS 75
+#define SML_MAX_VARS 50
 #define SML_REPLACE_VARS
 
 // tcp
 
-#define USE_TCP_BRIDGE
+//#define USE_TCP_BRIDGE
 
 // misc
 
 #define USE_SCRIPT_WEB_DISPLAY
 #define USE_SCRIPT_JSON_EXPORT
-#define MAX_METERS 1
+#define MAX_METERS 2
 #define MAXSVARS 10
 
 // eof
