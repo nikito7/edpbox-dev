1125ac735dc6f6b1252b60878f41b5081df3d07c
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-01-31 17:55:35 +0000

    Eb3-tri.js watchdog back

--- tasmota/Script-EB3-tri.js
+++ tasmota/Script-EB3-tri.js
@@ -1,13 +1,13 @@
 >D
 
-; EB3
-
-bug="fix"
 time=""
 date=""
 cnt=0
+clk=0
+wtd=0
+old=0
 
->BS
+>B
 
 tper=60
 smlj=0
@@ -24,7 +24,7 @@ endif
 
 if cnt==50
 then
-tper=30
+tper=60
 endif
 
 time=st(tstamp T 2)
@@ -35,11 +35,40 @@ then
 cnt+=1
 endif
 
+; modbus watchdog
+
+clk=sml[3]
+
+if cnt==99
+then
+wtd+=1
+endif
+
+if wtd==1
+then
+old=sml[3]
+endif
+
+if wtd==50
+then
+wtd=0
+if old==clk
+then
+print modbus error
+; 
+=>Restart -3
+; 
+endif
+endif
+
 >W
 
 @<b>NTP: </b> %date% %time%
 @<b>Vars: </b> cnt=%0cnt% tper=%0tper% smlj=%0smlj%
+@<b>Vars: </b> wtd=%0wtd% clk=%0clk% old=%0old%
 @<hr>
+
+; Below registers only work in EB3 !
 
 >M 1
 
