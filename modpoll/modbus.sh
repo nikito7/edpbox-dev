a9d6513bc24c8b6dba671693c2f7557952168840
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-03-27 03:38:09 +0100

    Update modbus.sh

--- modpoll/modbus.sh
+++ modpoll/modbus.sh
@@ -6,7 +6,7 @@ HOST="-m enc -p 9502 10.1.0.37"
 # serial
 #HOST="-m rtu /tty...."
 
-./modpoll  -a 1 -0 -1 -r 22 -c 1 -t 3:int -e $HOST 
+/config/apps/modpoll -a 1 -0 -1 -r 22 -c 1 -t 3:int -e $HOST 
 
 
 
