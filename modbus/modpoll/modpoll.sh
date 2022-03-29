e1ad3aa46e3ac543149f9d7a83f193a094ded977
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-03-30 00:17:52 +0100

    Update modpoll.sh

--- modbus/modpoll/modpoll.sh
+++ modbus/modpoll/modpoll.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
 # rtu over tcp
+
 HOST="-m enc -p 9502 10.1.0.37"
 
 # serial
-#TTY="/dev/ttyS3"
-#stty -F $TTY cs8 9600
-#HOST="-b 9600 -p none -m rtu $TTY"
 
-function four()
+TTY="/dev/ttyS3"
+stty -F $TTY cs8 9600
+HOST="-b 9600 -p none -m rtu $TTY"
+
+function get32bits()
 {
-/config/apps/modpoll -a 1 -0 -1 -r $1 -c $2 -t 3:int -e $HOST \
+/config/apps/modpoll -a 1 -0 -1 \
+-r $1 -c $2 -t 3:int -e $HOST \
 | grep '^\[' | awk -F ": " '{ print $2 "," }'
-
 }
 
-result=${result}$(four 38 3)
-result=${result}$(four 22 2)
+result=$(get32bits 38 3)
 
 echo $result | sed -e 's/ //g'
 
