636f1a7e73b86fc1b3a4cf8b17422800131bfbe4
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-05-05 17:33:52 +0100

    Update script-eb1-mono.js

--- tasmota/script-eb1-mono.js
+++ tasmota/script-eb1-mono.js
@@ -9,26 +9,14 @@ old=0
 
 >B
 
-tper=20
+tper=60
 smlj=0
 
-=>WiFi 0
 =>SerialLog 0
 =>SensorRetain 1
 =>Sensor53 r
 
 >S
-
-if cnt==10
-then
-=>WiFi 1
-tper=10
-endif
-
-if cnt==30
-then
-smlj=1
-endif
 
 time=st(tstamp T 2)
 date=st(tstamp T 1)
@@ -58,51 +46,51 @@ endif
 ; change to your gpios and mode
 ; power off is required
 ;  v  v             v
-+1,3,mN1,1,9600,EB1,1,12,r010400010001,r0104006C0002,r010400160002,r010400260003,r010400790003,r0104007F0002,r0104000B0002
++1,3,mN1,1,9600,EB1,1,15,r010400010001,r0104006C0002,r010400160002,r010400260003,r010400790003,r0104007F0002,r0104000B0002
 
 ; 01
 
-1,01040Cxxxxxxxxxxuu@i0:1,Clock ,h,Clock_H,0
-1,01040Cxxxxxxxxxxxxuu@i0:1,Clock ,m,Clock_M,0
-1,01040Cxxxxxxxxxxxxxxuu@i0:1,Clock ,s,Clock_S,0
+1,01040Cxxxxxxxxxxuu@i0:1,Clock ,h,Clock_H,16
+1,01040Cxxxxxxxxxxxxuu@i0:1,Clock ,m,Clock_M,16
+1,01040Cxxxxxxxxxxxxxxuu@i0:1,Clock ,s,Clock_S,16
 
 1,=h<hr>
 
 ; 6C
 
-1,010404UUuu@i1:10,Voltage L1 ,V,Voltage,1
-1,010404xxxxUUuu@i1:10,Current L1 ,A,Current,1
+1,010404UUuu@i1:10,Voltage L1 ,V,Voltage,17
+1,010404xxxxUUuu@i1:10,Current L1 ,A,Current,17
 
 1,=h<hr>
 
 ; 16
 
-1,010408UUuuUUuu@i2:1000,Energy Import ,kWh,Energy,2
-1,010408xxxxxxxxUUuuUUuu@i2:1000,Energy Export ,kWh,TotEneExp,2
+1,010408UUuuUUuu@i2:1000,Energy Import ,kWh,Energy,18
+1,010408xxxxxxxxUUuuUUuu@i2:1000,Energy Export ,kWh,TotEneExp,18
 
 1,=h<hr>
 
 ; 26
 
-1,01040CUUuuUUuu@i3:1000,Energy T1 Vazio ,kWh,TotEneT1,2
-1,01040CxxxxxxxxUUuuUUuu@i3:1000,Energy T2 Ponta ,kWh,TotEneT2,2
-1,01040CxxxxxxxxxxxxxxxxUUuuUUuu@i3:1000,Energy T3 Cheias ,kWh,TotEneT3,2
+1,01040CUUuuUUuu@i3:1000,Energy T1 Vazio ,kWh,TotEneT1,18
+1,01040CxxxxxxxxUUuuUUuu@i3:1000,Energy T2 Ponta ,kWh,TotEneT2,18
+1,01040CxxxxxxxxxxxxxxxxUUuuUUuu@i3:1000,Energy T3 Cheias ,kWh,TotEneT3,18
 
 1,=h<hr>
 
 ; 79
 
-1,01040aUUuuUUuu@i4:1,Active Power ,W,Active_Power,0
-1,01040axxxxxxxxUUuuUUuu@i4:1,Active Power Export ,W,ActPowExp,0
-1,01040axxxxxxxxxxxxxxxxUUuu@i4:1000,Power Factor ,pu,Power_Factor,3
+1,01040aUUuuUUuu@i4:1,Active Power ,W,Active_Power,16
+1,01040axxxxxxxxUUuuUUuu@i4:1,Active Power Export ,W,ActPowExp,16
+1,01040axxxxxxxxxxxxxxxxUUuu@i4:1000,Power Factor ,pu,Power_Factor,19
 
 ; 7F
 
-1,01040aUUuu@i5:10,Frequency ,Hz,Frequency,1
+1,01040aUUuu@i5:10,Frequency ,Hz,Frequency,17
 
 ; 0B
 
-1,010406uu@i6:1,Tariff ,,Tariff,0
+1,010406uu@i6:1,Tariff ,,Tariff,16
 
 1,=h<hr>
 
