8c4af4a10906f96d856e86153f29dd481377231d
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-05-05 17:02:08 +0100

    Update Script-EB1-mono.js

--- tasmota/Script-EB1-mono.js
+++ tasmota/Script-EB1-mono.js
@@ -39,31 +39,6 @@ cnt+=1
 endif
 
 ; modbus watchdog block begin
-
-clk=sml[3]
-
-if cnt==99
-then
-wtd+=1
-endif
-
-if wtd==1
-then
-old=sml[3]
-endif
-
-if wtd==50
-then
-wtd=0
-if old==clk
-then
-print modbus error
-; 
-=>Restart -3
-; 
-endif
-endif
-
 ; modbus watchdog block end
 
 >W
