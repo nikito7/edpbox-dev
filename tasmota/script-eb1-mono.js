beccad2ac2a4b2e3ec22f4c21aa18754befedcae
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-05-08 15:23:08 +0100

    Update script-eb1-mono.js

--- tasmota/script-eb1-mono.js
+++ tasmota/script-eb1-mono.js
@@ -12,7 +12,7 @@ ss=0
 
 >B
 
-tper=60
+tper=10
 smlj=0
 
 =>SerialLog 0
@@ -86,47 +86,47 @@ endif
 
 ; 01
 
-1,01040Cxxxxxxxxxxuu@i0:1,Clock ,h,Clock_H,16
-1,01040Cxxxxxxxxxxxxuu@i0:1,Clock ,m,Clock_M,16
-1,01040Cxxxxxxxxxxxxxxuu@i0:1,Clock ,s,Clock_S,16
+1,01040Cxxxxxxxxxxuu@i0:1,Clock ,h,Clock_H,0
+1,01040Cxxxxxxxxxxxxuu@i0:1,Clock ,m,Clock_M,0
+1,01040Cxxxxxxxxxxxxxxuu@i0:1,Clock ,s,Clock_S,0
 
 1,=h<hr>
 
 ; 6C
 
-1,010404UUuu@i1:10,Voltage L1 ,V,Voltage,17
-1,010404xxxxUUuu@i1:10,Current L1 ,A,Current,17
+1,010404UUuu@i1:10,Voltage L1 ,V,Voltage,1
+1,010404xxxxUUuu@i1:10,Current L1 ,A,Current,1
 
 1,=h<hr>
 
 ; 16
 
-1,010408UUuuUUuu@i2:1000,Energy Import ,kWh,Energy,19
-1,010408xxxxxxxxUUuuUUuu@i2:1000,Energy Export ,kWh,TotEneExp,19
+1,010408UUuuUUuu@i2:1000,Energy Import ,kWh,Energy,3
+1,010408xxxxxxxxUUuuUUuu@i2:1000,Energy Export ,kWh,TotEneExp,3
 
 1,=h<hr>
 
 ; 26
 
-1,01040CUUuuUUuu@i3:1000,Energy T1 Vazio ,kWh,TotEneT1,19
-1,01040CxxxxxxxxUUuuUUuu@i3:1000,Energy T2 Ponta ,kWh,TotEneT2,19
-1,01040CxxxxxxxxxxxxxxxxUUuuUUuu@i3:1000,Energy T3 Cheias ,kWh,TotEneT3,19
+1,01040CUUuuUUuu@i3:1000,Energy T1 Vazio ,kWh,TotEneT1,3
+1,01040CxxxxxxxxUUuuUUuu@i3:1000,Energy T2 Ponta ,kWh,TotEneT2,3
+1,01040CxxxxxxxxxxxxxxxxUUuuUUuu@i3:1000,Energy T3 Cheias ,kWh,TotEneT3,3
 
 1,=h<hr>
 
 ; 79
 
-1,01040aUUuuUUuu@i4:1,Active Power ,W,Active_Power,16
-1,01040axxxxxxxxUUuuUUuu@i4:1,Active Power Export ,W,ActPowExp,16
-1,01040axxxxxxxxxxxxxxxxUUuu@i4:1000,Power Factor ,pu,Power_Factor,19
+1,01040aUUuuUUuu@i4:1,Active Power ,W,Active_Power,0
+1,01040axxxxxxxxUUuuUUuu@i4:1,Active Power Export ,W,ActPowExp,0
+1,01040axxxxxxxxxxxxxxxxUUuu@i4:1000,Power Factor ,pu,Power_Factor,3
 
 ; 7F
 
-1,01040aUUuu@i5:10,Frequency ,Hz,Frequency,17
+1,01040aUUuu@i5:10,Frequency ,Hz,Frequency,1
 
 ; 0B
 
-1,010406uu@i6:1,Tariff ,,Tariff,16
+1,010406uu@i6:1,Tariff ,,Tariff,0
 
 1,=h<hr>
 
