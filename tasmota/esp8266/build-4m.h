83753bc1d0a5ebbc81071937fadc7b1b97d874e8
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-06-29 22:38:48 +0100

    Create build-4m.h

---
+++ tasmota/esp8266/build-4m.h
@@ -0,0 +1,34 @@
+// copy to user_config_override.h
+
+#define USE_ADC_VCC
+
+// tasmota4M
+
+#define USE_UFILESYS
+#define UFSYS_SIZE 8192
+
+// ### ### ###
+// ### ### ###
+
+// user_config_override.h
+// sml configs
+
+#undef USE_RULES
+#define USE_SCRIPT
+#define USE_SML_M
+#define USE_SML_SCRIPT_CMD
+#define SML_MAX_VARS 50
+#define SML_REPLACE_VARS
+
+// tcp
+
+#define USE_TCP_BRIDGE
+
+// misc
+
+#define USE_SCRIPT_WEB_DISPLAY
+#define USE_SCRIPT_JSON_EXPORT
+#define MAX_METERS 2
+#define MAXSVARS 10
+
+// eof
