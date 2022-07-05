6756e8a99f691aa2b8976e72581cd16a95cc08fa
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-07-05 20:11:44 +0100

    Update script-eb3-tri.js

--- tasmota/script-eb3-tri.js
+++ tasmota/script-eb3-tri.js
@@ -184,8 +184,9 @@ endif
 1,01441dxxxxxxuu@i7:1,Day,,L_D,0
 1,01441dxxxxxxxxxxuu@i7:1,Hour,h,L_HH,0
 1,01441dxxxxxxxxxxxxuu@i7:1,Minute,m,L_MM,0
-1,01441dxxxxxxxxxxxxxxxxxxxxxxUUuu@i7:1,Summer,,*,0
-1,01441dxxxxxcccxxxxxxxxxxxxxxxxxxUUuuUUuu@i7:1,Import Inc,Wh,L_IMP,0
+1,01441dxxxxxxxxxxxxxxxxxxxxxxuu@i7:1,Summer,,*,0
+1,01441dxxxxxxxxxxxxxxxxxxxxxxxxuu@i7:1,AMR Profile Status,,*,0
+1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i7:1,Import Inc,Wh,L_IMP,0
 1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i7:1,R1 Inc,VArh,*,0
 1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i7:1,Export Inc,Wh,L_EXP,0
 1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i7:1000,R2 Inc,kVArh,*,3
