e984202139c5359919ecebaa5ac028ce6b7bb1de
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-08-09 11:25:44 +0100

    Update zz-test.js

--- tasmota/zz-test.js
+++ tasmota/zz-test.js
@@ -1,3 +1,5 @@
+1,014411xxxxxxxxxxxxxxxxxxxxxxuu@i7:1,Summer,,LP1_DST,0
+1,014411xxxxxxxxxxxxxxxxxxxxxxxxuu@i7:1,AMR Profile Status,,LP2_AMR,0
 1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1,LP3 ,Wh,*,0
 1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1,LP4 ,Wh,*,0
 1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1,LP5 ,Wh,*,0
