c328f975a510f6088393da101d983a16a45d5e1d
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-08-12 21:10:44 +0100

    Update zz-pzem017.js

--- tasmota/zz-pzem017.js
+++ tasmota/zz-pzem017.js
@@ -9,7 +9,7 @@
 
 ; rx mode  baud     tx time
 ;  v  v     v        v  v
-+1,3,mN2,1,9600,pzem,1,30,r010400000006,r010400010006
++1,3,mN2,1,9600,pzem,1,30,r010400000002
 
 ; console debug
 ;
@@ -18,7 +18,8 @@
 
 1,=h<hr>
 
-1,01040CUUuu@i0:1,go to console ,,*,0
+1,01040CUUuu@i0:1,go to console ,V,*,0
+1,01040CxxxxUUuu@i0:1,go to console ,A,*,0
 
 1,=h<hr>
 
