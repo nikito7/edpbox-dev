e277f508d1aab12ff97fdf8f35e16a3e68d3f1bf
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-08-12 21:08:18 +0100

    Update and rename xx-pzem017.js to zz-pzem017.js

---
+++ tasmota/zz-pzem017.js
@@ -0,0 +1,27 @@
+>D
+>B
+
+=>SerialLog 0
+=>Sensor53 r
+=>Sensor53 d1
+
+>M 1
+
+; rx mode  baud     tx time
+;  v  v     v        v  v
++1,3,mN2,1,9600,pzem,1,30,r010400000006,r010400010006
+
+; console debug
+;
+; sensor53 d1
+; sensor53 d0
+
+1,=h<hr>
+
+1,01040CUUuu@i0:1,go to console ,,*,0
+
+1,=h<hr>
+
+#
+
+; eof
