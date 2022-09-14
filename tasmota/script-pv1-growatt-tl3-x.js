c861b0ba5d628673f39447b96337db1e677ae6ee
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-09-14 12:02:27 +0100

    Update script-pv1-growatt-tl3-x.js

--- tasmota/script-pv1-growatt-tl3-x.js
+++ tasmota/script-pv1-growatt-tl3-x.js
@@ -47,24 +47,29 @@ endif
 
 >M 1
 
-+1,3,mN1,1,9600,PVx,1,15,r010400000003,r010400370002,r0104005D0003
++1,3,mN1,1,9600,PVx,1,15,r010400000003,r010400370002,r010400270003,r0104005D0003
 
-; 0,1,2 0x0000
+; 0x0000 0,1,2
 
 1,010406UUuu@i0:1,Inverter Status,,PV_Status,0
 1,010406xxxxUUuuUUuu@i0:10,Input Power,W,*,1
 
-; 55-56 0x0037
+; 0x0027 35-36,37
 
-1,010404UUuuUUuu@i1:10,Total Energy,kWh,PV_Energy,1
+1,010406UUuuUUuu@i1:10,Output Power,W,*,1
+1,010406xxxxxxxxUUuu@i1:10,Frequency,Hz,*,2
+
+; 0x0037 55-56
+
+1,010404UUuuUUuu@i2:10,Total Energy,kWh,PV_Energy,1
 
 1,=h<br>
 
-; 93-95 0x005D
+; 0x005D 93-95
 
-1,010406UUuu@i2:10,Temp 1,°C,*,1
-1,010406xxxxUUuu@i2:10,Temp 2,°C,*,1
-1,010406xxxxxxxxUUuu@i2:10,Temp 3,°C,*,1
+1,010406UUuu@i3:10,Temp 1,°C,*,1
+1,010406xxxxUUuu@i3:10,Temp 2,°C,*,1
+1,010406xxxxxxxxUUuu@i3:10,Temp 3,°C,*,1
 
 ; eof meter
 
