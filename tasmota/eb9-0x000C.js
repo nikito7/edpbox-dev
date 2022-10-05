8b4baf29f672e99a22b986c57371760901b986af
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-10-05 20:13:07 +0100

    Update eb9-0x000C.js

--- tasmota/eb9-0x000C.js
+++ tasmota/eb9-0x000C.js
@@ -12,5 +12,9 @@
 
 1,01041CUuuuUUuu@i0:1,T1,VA,*,3
 1,01041Cx4UuuuUUuu@i0:1,T2,VA,*,3
+1,01041Cx8UuuuUUuu@i0:1,T3,VA,*,3
+1,01041Cx8x4UuuuUUuu@i0:1,T4,VA,*,3
+1,01041Cx8x8UuuuUUuu@i0:1,T5,VA,*,3
+1,01041Cx8x8x4UuuuUUuu@i0:1,T6,VA,*,3
 
 #
