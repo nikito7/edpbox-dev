ba1aba8d286bf4415c0ea52a178d435f03b8e4f6
nikito7 <45373783+nikito7@users.noreply.github.com>
2022-11-24 20:25:13 +0000

    Delete zz-test.js

--- tasmota/zz-test.js
+++
@@ -1,6 +0,0 @@
-1,01441dxxxxxxxxxxxxxxxxxxxxxxuu@i6:1,Summer,,LP1_DST,0
-1,01441dxxxxxxxxxxxxxxxxxxxxxxxxuu@i6:1,AMR Profile Status,,LP2_AMR,0
-1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1,LP3 ,Wh,*,0
-1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1,LP4 ,Wh,*,0
-1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1,LP5 ,Wh,*,0
-1,01441dxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxUUuuUUuu@i6:1,LP6 ,Wh,*,0
