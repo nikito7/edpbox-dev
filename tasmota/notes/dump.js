1111d32dbbe69c71336c06788063182965dd2786
nikito7 <45373783+nikito7@users.noreply.github.com>
2024-01-26 15:49:14 +0000

    Update dump.js

--- tasmota/notes/dump.js
+++ tasmota/notes/dump.js
@@ -1,45 +1,34 @@
 >D
 
-initvars="bugfix"
+vars="bugfix"
 hex=""
 cmd=""
 date=""
 time=""
 res=0
-sleep=0
 sec=0
 cnt=0
 now=0
-begin=4550
+begin=500
 end=0
 
 >B
 
 cnt=begin
 
-=>sensor53 d1
 =>sensor53 r
+=>sensor53 d1
 
->F
+>S
 
 if cnt>end
-and sleep==1
 then
 now=cnt
 hex=hx(cnt)
 cmd="014506"+hex+"01"
-res=sml(1 1 cmd)
+res=sml(1 3 cmd)
 cnt-=1
 endif
-
-sleep+=1
-
-if sleep>1
-then
-sleep=0
-endif
-
->S
 
 time=st(tstamp T 2)
 date=st(tstamp T 1)
@@ -60,5 +49,8 @@ endif
 
 >M 1
 +1,3,mN1,0,9600,Dump,1
+
+1,01040Cx4xxxxxxuu@i0:1,Clock ,s,CS,0
+
 #
 ; eof
