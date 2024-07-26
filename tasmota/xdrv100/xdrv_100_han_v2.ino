8dcce7999f8ad7237e15dd678ef24d479222d72d
nikito7 <45373783+nikito7@users.noreply.github.com>
2024-07-26 22:17:08 +0100

    Update xdrv_100_han_v2.ino

--- tasmota/xdrv100/xdrv_100_han_v2.ino
+++ tasmota/xdrv100/xdrv_100_han_v2.ino
@@ -6,7 +6,7 @@
 #warning **** HAN_V2 Driver is included... ****
 #define XDRV_100 100
 
-#define HAN_VERSION_T "14.1.0-7.23.5b4"
+#define HAN_VERSION_T "14.1.0-7.23.5b5"
 
 #ifdef EASYHAN_TCP
 #undef HAN_VERSION
