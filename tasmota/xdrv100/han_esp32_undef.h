6bab4b856c3a88a92d40a1f9847f5b7f08df30e0
nikito7 <45373783+nikito7@users.noreply.github.com>
2024-09-12 15:10:12 +0100

    Update han_esp32_undef.h

--- tasmota/xdrv100/han_esp32_undef.h
+++ tasmota/xdrv100/han_esp32_undef.h
@@ -1,1 +1,18 @@
+#ifndef _HAN_ESP32_UNDEF_H_
+#define _HAN_ESP32_UNDEF_H_
 
+// edit > include/tasmota_configurations_ESP32
+// ..
+// [tasmota32.bin]
+// ..
+//  #include ../han_esp32_undef.h
+
+#undef USE_ENHANCED_GUI_WIFI_SCAN
+#undef USE_EMULATION
+#undef USE_TIMERS
+#undef USE_TIMERS_WEB
+#undef USE_DOMOTICZ
+
+// ### ### ###
+
+#endif // _HAN_ESP32_UNDEF_H_
