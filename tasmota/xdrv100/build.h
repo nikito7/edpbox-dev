cb632e0bfd3c622c7eb45273102fde184c1bbbed
nikito7 <45373783+nikito7@users.noreply.github.com>
2024-09-14 02:45:45 +0100

    Update build.h

--- tasmota/xdrv100/build.h
+++ tasmota/xdrv100/build.h
@@ -6,7 +6,7 @@
 
 // ### ### ###
 // ### ### ###
-// changes 2024.09.03
+// changes 2024.09.12
 
 #ifdef ESP32S3
 #define USE_LD2410 // radar
@@ -83,6 +83,15 @@
 #define LARGE_ARRAYS
 #define SCRIPT_LARGE_VNBUFF
 #define SCRIPT_MAXSSIZE 128
+#define USE_FEXTRACT
+#undef MAXVARS
+#define MAXVARS 100
+#undef MAXSVARS
+#define MAXSVARS 50
+#undef MAXFILT
+#define MAXFILT 10
+#undef SML_MAX_VARS
+#define SML_MAX_VARS 100
 #endif
 
 #ifndef HAN_V1
@@ -101,17 +110,46 @@
 
 // Default Configs
 
+#undef  APP_TIMEZONE
+#define APP_TIMEZONE 99
+
+#undef  TIME_DST_HEMISPHERE
+#define TIME_DST_HEMISPHERE    0
+#undef  TIME_DST_WEEK
+#define TIME_DST_WEEK          0
+#undef  TIME_DST_MONTH
+#define TIME_DST_MONTH         3
+#undef  TIME_DST_DAY
+#define TIME_DST_DAY           1
+#undef  TIME_DST_HOUR
+#define TIME_DST_HOUR          1
+#undef  TIME_DST_OFFSET
+#define TIME_DST_OFFSET        60
+
+#undef  TIME_STD_HEMISPHERE
+#define TIME_STD_HEMISPHERE    0
+#undef  TIME_STD_WEEK
+#define TIME_STD_WEEK          0
+#undef  TIME_STD_MONTH
+#define TIME_STD_MONTH         10
+#undef  TIME_STD_DAY
+#define TIME_STD_DAY           1
+#undef  TIME_STD_HOUR
+#define TIME_STD_HOUR          2
+#undef  TIME_STD_OFFSET
+#define TIME_STD_OFFSET        0
+
 // clang-format off
 
 #if defined(ESP8266)
 #undef USER_BACKLOG
-#define USER_BACKLOG "TimeZone 99; TimeDST 0,0,3,1,1,60; TimeSTD 0,0,10,1,2,0; WebLog 2; Sleep 75; WifiPower 15; Template {\"NAME\":\"easyhan.pt\",\"GPIO\":[1,1,1,1,1,1,1,1,1,1,1,1,1,1],\"FLAG\":0,\"BASE\":18}; Module 0; SaveData 9; SerialLog 0; WifiConfig 2; Script 1"
+#define USER_BACKLOG "WebLog 2; Sleep 75; WifiPower 15; Template {\"NAME\":\"easyhan.pt\",\"GPIO\":[1,1,1,1,1,1,1,1,1,1,1,1,1,1],\"FLAG\":0,\"BASE\":18}; Module 0; SaveData 9; SerialLog 0; WifiConfig 2; Script 1"
 //
 #elif defined(ESP32C6)
-#define USER_BACKLOG "TimeZone 99; TimeDST 0,0,3,1,1,60; TimeSTD 0,0,10,1,2,0; WebLog 4; Sleep 75; WifiPower 15; Template {\"NAME\":\"aht.c6.easyhan.pt\",\"GPIO\":[1,1,1,1,1,1,640,608,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0],\"FLAG\":0,\"BASE\":1}; Module 0; SaveData 9; WifiConfig 2; I2cDriver12 0; Script 1"
+#define USER_BACKLOG "WebLog 2; Sleep 75; WifiPower 15; Template {\"NAME\":\"aht.c6.easyhan.pt\",\"GPIO\":[1,1,1,1,1,1,640,608,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,1,0,0,0,0,0,0,0],\"FLAG\":0,\"BASE\":1}; Module 0; SaveData 9; WifiConfig 2; I2cDriver12 0; Script 1"
 //
 #else
-#define USER_BACKLOG "TimeZone 99; TimeDST 0,0,3,1,1,60; TimeSTD 0,0,10,1,2,0; WebLog 4; Sleep 75; WifiPower 15; SaveData 9; WifiConfig 2; Script 1"
+#define USER_BACKLOG "WebLog 2; Sleep 75; WifiPower 15; SaveData 9; WifiConfig 2; Script 1"
 #endif
 
 // clang-format on
