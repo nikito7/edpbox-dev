9820254908db763c965d23c802b3ce928fe3e189
nikito7 <45373783+nikito7@users.noreply.github.com>
2024-09-17 16:09:43 +0100

    Update flash.bat

--- tasmota/xdrv100/flash.bat
+++ tasmota/xdrv100/flash.bat
@@ -7,7 +7,8 @@ esptool erase_flash
 echo.
 echo write flash
 echo.
-esptool -b 921600 write_flash 0x0 tasmota32c6.factory.bin 0x3b0000 littlefs-esp32.bin
+esptool -b 921600 write_flash 0x0 tasmota32c6.factory.bin
+rem 0x3b0000 littlefs-esp32.bin
 echo.
 title done flash
 rem EOF %time%
