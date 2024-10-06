7d28d2736785cf26925509550794184dbbf564ec
nikito7 <45373783+nikito7@users.noreply.github.com>
2024-10-06 19:22:02 +0100

    Update han_esp32.h

--- tasmota/xdrv100/han_esp32.h
+++ tasmota/xdrv100/han_esp32.h
@@ -8,10 +8,8 @@
 //  #include ../han_esp32.h
 
 #undef USE_ENHANCED_GUI_WIFI_SCAN
-#undef USE_EMULATION
-#undef USE_TIMERS
 #undef USE_TIMERS_WEB
-#undef USE_DOMOTICZ
+#undef USE_AUTOCONF
 
 // ### ### ###
 
