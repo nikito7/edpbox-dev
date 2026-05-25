809e6acf9ea04f76580527125eb973dec0e2bd7b
nikito7 <45373783+nikito7@users.noreply.github.com>
2026-05-25 13:26:31 +0100

    Create han_esp32.h

---
+++ tasmota/xdrv100/han_esp32.h
@@ -0,0 +1,19 @@
+#ifndef _HAN_ESP32_H_
+#define _HAN_ESP32_H_
+
+// edit > include/tasmota_configurations_ESP32
+// ..
+// [tasmota32.bin]
+// ..
+//  #include ../han_esp32.h
+
+#undef USE_ENHANCED_GUI_WIFI_SCAN
+#undef USE_TIMERS_WEB
+#undef USE_AUTOCONF
+
+#undef INFLUXDB_INITIAL
+#define INFLUXDB_INITIAL 51
+
+// ### ### ###
+
+#endif // _HAN_ESP32_H_
